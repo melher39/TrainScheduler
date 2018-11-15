cb5eccc671f2952fcbfd887160b7472ab86b71dd
Melvin Hernandez <melher39@gmail.com>
2018-11-15 15:37:25 -0600

    linked firebase database + created basic app layout in html using bootstrap

---
+++ assets/javascript/logic.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+// initialize Firebase
+var config = {
+    apiKey: "AIzaSyAxSpkD_VEUOHcxmsSwRkD9x_-EK8mMLro",
+    authDomain: "train-scheduler-79a71.firebaseapp.com",
+    databaseURL: "https://train-scheduler-79a71.firebaseio.com",
+    projectId: "train-scheduler-79a71",
+    storageBucket: "train-scheduler-79a71.appspot.com",
+    messagingSenderId: "770731075832"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+});
