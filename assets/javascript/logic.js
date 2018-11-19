038c9dc5a78f51a1a01e6f51a8189393bdd4bd65
Melvin Hernandez <melher39@gmail.com>
2018-11-19 16:27:43 -0600

    added moment.js calculations for train times and they seem to be working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -38,11 +38,38 @@ $("#submit-button").on("click", function(event){
     console.log(firstTrainTime);
     console.log(trainFrequency);
 
+    // first train time converted to military time and pushed back 1 year so it comes before the current time
+    var firstTrainTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+    console.log("the first train time is:" + firstTrainTimeConverted);
+
+    // difference between the times in minutes
+    var timeDifference = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    console.log("time difference:" + timeDifference);
+
+    // time apart between train time and current time
+    var timeApart = timeDifference % trainFrequency;
+    console.log(timeApart);
+
+    // how many minutes until next train
+    var minutesTilNextTrain = trainFrequency - timeApart;
+    console.log("how long until next train:" + minutesTilNextTrain);
+
+    // next train will arrive at this time in minutes
+    var nextTrain = moment().add(minutesTilNextTrain, "minutes");
+    console.log("next train will arrive:" + nextTrain);
+
+    // next train will arrive at this time in hour format
+    var arrivalTime = moment(nextTrain).format("hh:mm");
+    console.log("this is the final time:" + arrivalTime);
+
+
+
     // upload the user-input values to the database
     database.ref().push({
         name: trainName,
         destination: destination,
-        time: firstTrainTime,
+        next: arrivalTime,
+        minutesAway: minutesTilNextTrain,
         frequency: trainFrequency
     });
 
@@ -66,8 +93,8 @@ database.ref().on("child_added", function(snapshot){
         $("<td>").text(sv.name),
         $("<td>").text(sv.destination),
         $("<td>").text(sv.frequency),
-        $("<td>").text(sv.time),
-        $("<td>").text(sv.time)
+        $("<td>").text(sv.next),
+        $("<td>").text(sv.minutesAway)
     );
 
     // display the new row under their respective columns
